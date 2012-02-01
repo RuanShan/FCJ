840507c7978342d56f46a15ac6792a158776600d
qing he <areq22@gmail.com>
2012-02-01 15:43:21 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fcj::Application.config.secret_token = '622c6c611de0cc2fd22c5a78712a519d6debc4c734d1679cfec44bc4175e5fab18c9af97c919217030df2c150804d1395301372babe5286accb36d4266fdda08'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Fcj::Application.config.session_store :cookie_store, :key => '_fcj_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Fcj::Application.config.session_store :active_record_store
