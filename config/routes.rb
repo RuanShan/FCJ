2744a7b375d3f63dc7ad54b737c6027dc75ae8ba
qing he <areq22@gmail.com>
2012-02-03 14:58:54 +0800

    add function dict

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Fcj::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
+     match 'dict' => 'welcome#dictionary'
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
