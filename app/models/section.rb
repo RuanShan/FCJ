0616f45b481362c0bb56aa5f83f0820c4281537d
unknown <root@.(none)>
2012-02-08 23:01:39 +0800

    fix entry for foreign_key

--- app/models/section.rb
+++ app/models/section.rb
@@ -5,5 +5,5 @@ class Section < ActiveRecord::Base
   self.new_cols=["NO","DistrictID","Name","CreateDate",["Address"],["Memo"]]
   self.edit_cols=["NO","DistrictID","Name","CreateDate",["Address"],["Memo"]]
   
-  
+  belongs_to "district", :foreign_key=>"DistrictID"
 end
